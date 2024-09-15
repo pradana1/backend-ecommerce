885f2220c82becd292d46e501426f4478750ea8b
pradana1 <pradanayp19@gmail.com>
2024-09-16 00:02:18 +0700

    second commit

---
+++ database/migrations/2024_09_12_154819_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('invoice_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('qty');
+            $table->bigInteger('price');
+            $table->timestamps();
+
+            //relationship table invoice
+            $table->foreign('invoice_id')->references('id')->on('invoices');
+
+            //relationship table product
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+
+    //route login
+    Route::post('/login', [App\Http\Controllers\Api\Admin\LoginController::class, 'index', ['as' => 'admin']]);
+
+    //group route with middleware "auth:api_admin"
+    Route::group(['middleware' => 'auth:api_admin'], function() {
+
+        //data user
+        Route::get('/user', [App\Http\Controllers\Api\Admin\LoginController::class, 'getUser', ['as' => 'admin']]);
+
+        //refresh token JWT
+        Route::get('/refresh', [App\Http\Controllers\Api\Admin\LoginController::class, 'refreshToken', ['as' => 'admin']]);
+
+        //logout
+        Route::post('/logout', [App\Http\Controllers\Api\Admin\LoginController::class, 'logout', ['as' => 'admin']]);
+    
+        //dashboard
+        Route::get('/dashboard', [App\Http\Controllers\Api\Admin\DashboardController::class, 'index', ['as' => 'admin']]);
+            
+        //categories resource
+        Route::apiResource('/categories', App\Http\Controllers\Api\Admin\CategoryController::class, ['except' => ['create', 'edit'], 'as' => 'admin']);
+    
+        //products resource
+        Route::apiResource('/products', App\Http\Controllers\Api\Admin\ProductController::class, ['except' => ['create', 'edit'], 'as' => 'admin']);
+    
+    });
+
+    
+
+});
